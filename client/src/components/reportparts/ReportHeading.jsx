0626de924a64983ff7245879563b2c2d5dde90bb
AbuzarZaidi <abuzarzaidi947@gmail.com>
2022-05-11 11:31:31 +0500

    change current amount position

--- client/src/components/reportparts/ReportHeading.jsx
+++ client/src/components/reportparts/ReportHeading.jsx
@@ -4,31 +4,27 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
-import Fab from '@mui/material/Fab';
-import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import Fab from "@mui/material/Fab";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 const ReportHeading = (props) => {
   return (
     <>
-    <Box  sx={{ display: "flex",justifyContent: 'flex-end', mt:1  }}>
-     <Fab variant="extended" >
-     <Typography
-                sx={{  color: "text.secondary", fontWeight: 600 }}
-              >
-                Current Amount:
-                
-       <AttachMoneyIcon  />{props.currentAmount}
-       </Typography>
-      </Fab>
-      </Box>
       <h1 className="display-2 d-flex  justify-content-center mb-5">
         Monthly Report
       </h1>
-      
+
       <div>
         <Box sx={{ display: "flex", justifyContent: "center" }}>
-        
           <List sx={{ mb: 1, width: "70%" }}>
-         
+            <Box sx={{ display: "flex", justifyContent: "flex-end", mt: 1 }}>
+              <Fab variant="extended">
+                <Typography sx={{ color: "text.secondary", fontWeight: 600 }}>
+                  Current Amount:
+                  <AttachMoneyIcon />
+                  {props.currentAmount}
+                </Typography>
+              </Fab>
+            </Box>
             <AccordionSummary>
               <Typography
                 sx={{ width: "10%", color: "text.secondary", fontWeight: 600 }}
