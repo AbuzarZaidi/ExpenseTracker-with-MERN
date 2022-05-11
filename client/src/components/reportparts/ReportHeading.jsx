73f7f7d41114fb130fd15e18048ec77fd8bfcf5c
AbuzarZaidi <abuzarzaidi947@gmail.com>
2022-05-11 14:23:07 +0500

    Filter added for type of entry

--- client/src/components/reportparts/ReportHeading.jsx
+++ client/src/components/reportparts/ReportHeading.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import {useState} from "react"
 import AccordionSummary from "@mui/material/AccordionSummary";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
@@ -6,7 +7,15 @@ import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
 import Fab from "@mui/material/Fab";
 import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 const ReportHeading = (props) => {
+  const [selectValue, setSelectValue] = useState("All");
+  const handleChange = (event) => {
+     setSelectValue(event.target.value);
+props.changeHandler(event.target.value)
+  };
   return (
     <>
       <h1 className="display-2 d-flex  justify-content-center mb-5">
@@ -16,13 +25,29 @@ const ReportHeading = (props) => {
       <div>
         <Box sx={{ display: "flex", justifyContent: "center" }}>
           <List sx={{ mb: 1, width: "70%" }}>
-            <Box sx={{ display: "flex", justifyContent: "flex-end", mt: 1 }}>
-              <Fab variant="extended">
+          
+            <Box sx={{ display: "flex", mt: 1 ,justifyContent: 'space-between'}}>
+            <FormControl sx={{  mb: 1, minWidth: 120,  }} >
+          <Select
+                value={selectValue}
+            onChange={handleChange}
+            displayEmpty
+            inputProps={{ "aria-label": "Without label" }}
+          >
+            <MenuItem value={"All"}>All</MenuItem>
+            <MenuItem value={"Deposit"}>Deposit</MenuItem>
+            <MenuItem value={"Withdraw"}>Withdraw</MenuItem>
+          </Select>
+          
+        </FormControl>
+              <Fab variant="extended" >
+           
                 <Typography sx={{ color: "text.secondary", fontWeight: 600 }}>
                   Current Amount:
                   <AttachMoneyIcon />
                   {props.currentAmount}
                 </Typography>
+               
               </Fab>
             </Box>
             <AccordionSummary>
--- client/src/pages/Reports.jsx
+++ client/src/pages/Reports.jsx
@@ -1,95 +1,71 @@
-import {useState,useEffect} from 'react';
+import { useState, useEffect } from "react";
 import * as React from "react";
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 import ReportHeading from "../components/reportparts/ReportHeading";
 import ReportContent from "../components/reportparts/ReportContent";
-// import InputLabel from '@mui/material/InputLabel';
-// import MenuItem from '@mui/material/MenuItem';
-// import FormHelperText from '@mui/material/FormHelperText';
-// import FormControl from '@mui/material/FormControl';
-// import Select from '@mui/material/Select';
+
 const { readExpense } = require("../functions/index");
 
 const Reports = () => {
-  const [expensesList,setExpensesList]=useState(null);
-  const [flag,setFlag]=useState(false);
-//    const [age, setAge] = React.useState('');
- const [currentAmount,setCurrentAmount]=useState(0);
-//   const handleChange = (event) => {
-//     setAge(event.target.value);
-//   };
+  const [expensesList, setExpensesList] = useState(null);
+  const [flag, setFlag] = useState(false);
+  const [filter, setFilter] = useState("All");
+  const [currentAmount, setCurrentAmount] = useState(0);
+ const FilterHanlder=(value)=>{
+  setFilter(value);
+ }
   useEffect(() => {
     const fetchData = async () => {
       const result = await readExpense();
-      
-      let amount=0;
-      for(let i=0;i<result.length;i++){
-        if(result[i].type==="Deposit"){
-          amount+=result[i].amount;
-        }
-        else if(result[i].type==="Withdraw")
-        amount-=result[i].amount;
+
+      let amount = 0;
+      for (let i = 0; i < result.length; i++) {
+        if (result[i].type === "Deposit") {
+          amount += result[i].amount;
+        } else if (result[i].type === "Withdraw") amount -= result[i].amount;
       }
-      setCurrentAmount(amount)
+      setCurrentAmount(amount);
       setFlag(true);
-      
-        setExpensesList(result);
-      
-     
+
+      setExpensesList(result);
     };
     fetchData();
-  }, [ setExpensesList]);
+  }, [setExpensesList, setFilter]);
   return (
     <>
-     {/* <div>
-      <FormControl sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-simple-select-helper-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-helper-label"
-          id="demo-simple-select-helper"
-          value={age}
-          label="Age"
-          onChange={handleChange}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-        <FormHelperText>With label + helper text</FormHelperText>
-      </FormControl>
-      <FormControl sx={{ m: 1, minWidth: 120 }}>
-        <Select
-          value={age}
-          onChange={handleChange}
-          displayEmpty
-          inputProps={{ 'aria-label': 'Without label' }}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-        <FormHelperText>Without label</FormHelperText>
-      </FormControl>
-    </div> */}
-      <ReportHeading  currentAmount={currentAmount}/>
-      
-      {/* <ReportContent  expense={expensesList}/> */}
-{!flag&&
-<Box sx={{ display: 'flex' , justifyContent: 'center'}}>
-      <CircularProgress />
-    </Box>}
-    {flag&&expensesList.length===0&& <p className="display-6 d-flex  justify-content-center mb-5">Could find any data!</p>}
-      {flag&&expensesList.length>0&&expensesList.map((expense,ind)=>{
-      return <ReportContent index={ind+1} key={expense._id} date={expense.date}  amount={expense.amount} type={expense.type} description={expense.description}/>
-      
-      })}
+     
+      <ReportHeading currentAmount={currentAmount} changeHandler={FilterHanlder} />
+
+     
+      {!flag && (
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <CircularProgress />
+        </Box>
+      )}
+      {flag && expensesList.length === 0 && (
+        <p className="display-6 d-flex  justify-content-center mb-5">
+          Could find any data!
+        </p>
+      )}
+      {flag &&
+        expensesList.length > 0 &&
+        expensesList
+          .filter((expense) =>
+            filter === "All" ? expense.type !== filter : expense.type === filter
+          )
+          .map((expense, ind) => {
+            return (
+              <ReportContent
+                index={ind + 1}
+                key={expense._id}
+                date={expense.date}
+                amount={expense.amount}
+                type={expense.type}
+                description={expense.description}
+              />
+            );
+          })}
     </>
   );
 };
