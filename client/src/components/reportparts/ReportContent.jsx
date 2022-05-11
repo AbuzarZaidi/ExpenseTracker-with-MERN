fe87a6f774fdff8744c5bed98d50cf92dfb0d744
AbuzarZaidi <abuzarzaidi947@gmail.com>
2022-05-11 10:30:16 +0500

    date added

--- client/src/components/reportparts/ReportContent.jsx
+++ client/src/components/reportparts/ReportContent.jsx
@@ -28,7 +28,7 @@ const ReportContent = (props) => {
             <Typography sx={{ width: "10%", flexShrink: 0 }}>1</Typography>
             <Typography sx={{ width: "30%", flexShrink: 0 }}>${props.amount}</Typography>
             <Typography sx={{ width: "29%" }}>{props.type}</Typography>
-            <Typography sx={{ width: "30%" }}>5/11/2022</Typography>
+            <Typography sx={{ width: "30%" }}>{props.date.split('T')[0]}</Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography sx={{color: "text.secondary", fontWeight: 600}}>
